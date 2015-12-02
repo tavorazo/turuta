3546dc95108f263a7c75791c7b94ad5b668bbe88
Octavio <octavio.razo.verduzco@gmail.com>
2015-12-01 22:37:17 -0600

    front

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <!--[if IE]><meta http-equiv="x-ua-compatible" content="IE=9" /><![endif]-->
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>TuRuta - No te quedes sin qué hacer</title>
+    <title>TuRuta | Guanajuato</title>
     <meta name="description" content="Spirit8 is a Digital agency one page template built based on bootstrap framework. This template is design by Robert Berki and coded by Jenn Pereira. It is simple, mobile responsive, perfect for portfolio and agency websites. Get this for free exclusively at ThemeForces.com">
     <meta name="keywords" content="bootstrap theme, portfolio template, digital agency, onepage, mobile responsive, spirit8, free website, free theme, themeforces themes, themeforces wordpress themes, themeforces bootstrap theme">
     <meta name="author" content="ThemeForces.com">
@@ -31,6 +31,8 @@
     ================================================== -->
     <link rel="stylesheet" type="text/css"  href="css/style.css">
     <link rel="stylesheet" type="text/css" href="css/responsive.css">
+    
+    <link type="text/css" href="css/turuta.css" rel="stylesheet">
 
     <link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,100italic,300italic,400italic,700italic,900italic' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,700,300,600,800,400' rel='stylesheet' type='text/css'>
@@ -63,13 +65,13 @@
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
           <ul class="nav navbar-nav navbar-right">
-            <li><a href="#tf-home" class="page-scroll">Home</a></li>
-            <li><a href="#tf-about" class="page-scroll">About</a></li>
-            <li><a href="#tf-team" class="page-scroll">Team</a></li>
-            <li><a href="#tf-services" class="page-scroll">Services</a></li>
-            <li><a href="#tf-works" class="page-scroll">Portfolio</a></li>
-            <li><a href="#tf-testimonials" class="page-scroll">Testimonials</a></li>
-            <li><a href="#tf-contact" class="page-scroll">Contact</a></li>
+            <li><a href="#tf-home" class="page-scroll">Inicio</a></li>
+            <li><a href="#tf-about" class="page-scroll">Como funciona?</a></li>
+            <li><a href="#tf-team" class="page-scroll">Equipo</a></li>
+            <li><a href="#tf-services" class="page-scroll">En la app</a></li>
+            <li><a href="#tf-works" class="page-scroll">Ofertas</a></li>
+            <li><a href="#tf-testimonials" class="page-scroll">Testimonios</a></li>
+            <li><a href="#tf-contact" class="page-scroll">Contactanos</a></li>
           </ul>
         </div><!-- /.navbar-collapse -->
       </div><!-- /.container-fluid -->
@@ -81,7 +83,7 @@
         <div class="overlay">
             <div class="content">
                 <h1>Disfruta <strong><span class="color">Guanajuato</span></strong></h1>
-                <p class="lead">Te ofrecemos una <strong>ruta personalizada</strong> llena de <strong>qué ver y qué</strong></p>
+                <p class="lead">Una <strong>ruta personalizada</strong> llena de <strong> aventura.</strong></p>
                 <a href="#tf-about" class="fa fa-angle-down page-scroll"></a>
             </div>
         </div>
@@ -93,29 +95,38 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-6">
-                    <img src="img/02.png" class="img-responsive">
+                    <img src="http://www.italyguides.it/images/italia/eng/iphone-app/mano-grande.jpg" id="img" style="width:300px">
                 </div>
                 <div class="col-md-6">
                     <div class="about-text">
                         <div class="section-title">
-                            <h4>About us</h4>
-                            <h2>Some words <strong>about us</strong></h2>
+                            <h4>Vive</h4>
+                            <h2 style="zoom:90%"> la mejor experiencia <strong>de viaje</strong></h2>
                             <hr>
                             <div class="clearfix"></div>
                         </div>
-                        <p class="intro">We love building and rebuilding brands through our  specific skills. Using colour, fonts, and illustration, we brand companies in a way they will never forget.</p>
+                        <p class="intro">
+                            Vive una experiencia inolvidable conociendo lugares que estamos seguros te van a gustar.
+                            <br><br>
+                            Nos aseguramos de que el itinerario otorgado sea el mas optimo para que disfrutes y conozcas la mayor cantidad de lugares que son de tu interes.
+                            <br><br>
+                        </p>
                         <ul class="about-list">
                             <li>
                                 <span class="fa fa-dot-circle-o"></span>
-                                <strong>Mission</strong> - <em>We deliver uniqueness and quality</em>
+                                <strong>Descarga</strong>  <em>la app</em>.
                             </li>
                             <li>
                                 <span class="fa fa-dot-circle-o"></span>
-                                <strong>Skills</strong> - <em>Delivering fast and excellent results</em>
+                                <strong>Selecciona</strong> <em>tu destino</em>.
                             </li>
                             <li>
                                 <span class="fa fa-dot-circle-o"></span>
-                                <strong>Clients</strong> - <em>Satisfied clients thanks to our experience</em>
+                                <strong>Cuentanos</strong> <em>de ti y de lo que te gusta</em>.
+                            </li>
+                            <li>
+                                <span class="fa fa-dot-circle-o"></span>
+                                <strong>Vive</strong> <em>la mejor experiencia en tu destino.</em>.
                             </li>
                         </ul>
                     </div>
@@ -130,14 +141,14 @@
         <div class="overlay">
             <div class="container">
                 <div class="section-title center">
-                    <h2>Meet <strong>our team</strong></h2>
+                    <h2>Nuestro <strong>equipo</strong></h2>
                     <div class="line">
                         <hr>
                     </div>
                 </div>
 
                 <div id="team" class="owl-carousel owl-theme row">
-                    <div class="item">
+                    <!--div class="item">
                         <div class="thumbnail">
                             <img src="img/team/01.jpg" alt="..." class="img-circle team-img">
                             <div class="caption">
@@ -190,37 +201,37 @@
                                 <p>Do not seek to change what has come before. Seek to create that which has not.</p>
                             </div>
                         </div>
-                    </div>
+                    </div-->
 
                     <div class="item">
                         <div class="thumbnail">
-                            <img src="img/team/01.jpg" alt="..." class="img-circle team-img">
+                            <img src="https://avatars3.githubusercontent.com/u/9580544?v=3&s=460" alt="..." class="img-circle team-img">
                             <div class="caption">
-                                <h3>Jenn Gwapa</h3>
+                                <h3>Eduardo Velez</h3>
                                 <p>CEO / Founder</p>
-                                <p>Do not seek to change what has come before. Seek to create that which has not.</p>
+                                <p>full stack web developer.</p>
                             </div>
                         </div>
                     </div>
 
                     <div class="item">
                         <div class="thumbnail">
-                            <img src="img/team/02.jpg" alt="..." class="img-circle team-img">
+                            <img src="https://avatars1.githubusercontent.com/u/8703256?v=3&s=460" alt="..." class="img-circle team-img">
                             <div class="caption">
-                                <h3>Jenn Gwapa</h3>
+                                <h3>Octavio Razo</h3>
                                 <p>CEO / Founder</p>
-                                <p>Do not seek to change what has come before. Seek to create that which has not.</p>
+                                <p>full stack web developer.</p>
                             </div>
                         </div>
                     </div>
 
                     <div class="item">
                         <div class="thumbnail">
-                            <img src="img/team/03.jpg" alt="..." class="img-circle team-img">
+                            <img src="https://avatars1.githubusercontent.com/u/5150392?v=3&s=460" alt="..." class="img-circle team-img">
                             <div class="caption">
-                                <h3>Jenn Gwapa</h3>
+                                <h3>Raul Medina</h3>
                                 <p>CEO / Founder</p>
-                                <p>Do not seek to change what has come before. Seek to create that which has not.</p>
+                                <p>full stack web developer.</p>
                             </div>
                         </div>
                     </div>
@@ -235,7 +246,7 @@
     <div id="tf-services" class="text-center">
         <div class="container">
             <div class="section-title center">
-                <h2>Take a look at <strong>our services</strong></h2>
+                <h2><strong>Caracteristicas</strong> de la app.</h2>
                 <div class="line">
                     <hr>
                 </div>
@@ -244,7 +255,7 @@
             </div>
             <div class="space"></div>
             <div class="row">
-                <div class="col-md-3 col-sm-6 service">
+                <!--div class="col-md-3 col-sm-6 service">
                     <i class="fa fa-desktop"></i>
                     <h4><strong>Web design</strong></h4>
                     <p>The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
@@ -266,6 +277,56 @@
                     <i class="fa fa-bullhorn"></i>
                     <h4><strong>Marketing</strong></h4>
                     <p>The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
+                </div-->
+                <div>
+                    <div id="left">
+                        <div class="feature">
+                            <img src="https://cdn2.iconfinder.com/data/icons/app-types-in-grey/128/food_and_drink_512pxGREY.png" alt="">
+                            <h2>Restaurants</h2>
+                            <p id="p-left">Comida unica para ti.</p>
+                        </div>
+                        <div class="feature">
+                            <img src="https://cdn4.iconfinder.com/data/icons/adiante-apps-app-templates-incos-in-grey/128/app_type_museum_512px_GREY.png" alt="">
+                            <h2>Museos</h2>
+                            <p id="p-left">Conoce mas sobre nuestro estado.</p>
+                        </div>
+                        <div class="feature">
+                            <img src="https://cdn2.iconfinder.com/data/icons/app-types-in-grey/128/app_type_festival_512px_GREY.png" alt="">
+                            <h2>Vida Nocturna</h2>
+                            <p id="p-left">La mejor fiesta para todos los gustos.</p>
+                        </div>
+                        <div class="feature">
+                            <img src="https://cdn4.iconfinder.com/data/icons/adiante-apps-app-templates-incos-in-grey/128/app_type_spa_512px_GREY.png" alt="">
+                            <h2>Recreación</h2>
+                            <p id="p-left">La mejor fiesta para todos los gustos.</p>
+                        </div>
+
+                    </div>
+                    <div id="phone">
+                        <img src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRbBfKVH_q37_XSxkWPZgZLYtCI882Aa5xkAR9zrmw0vyfCqF4e" alt="">
+                    </div>
+                    <div id="right">
+                        <div class="feature">
+                            <img src="https://cdn4.iconfinder.com/data/icons/adiante-apps-app-templates-incos-in-grey/128/app_type_real_state_512px_GREY.png" alt="">
+                            <h2>Hoteles</h2>
+                            <p id="p-right">Los mejores lugares donde hospedarte</p>
+                        </div>
+                        <div class="feature">
+                            <img src="https://cdn1.iconfinder.com/data/icons/sports-glyphs-3/128/116-64.png">
+                            <h2>Zonas extremo</h2>
+                            <p id="p-right">Los mejores lugares al aire libre.</p>
+                        </div>
+                        <div class="feature">
+                            <img src="https://cdn2.iconfinder.com/data/icons/app-types-in-grey/128/app_type_mall_512px_GREY.png" alt="">
+                            <h2>Tiendas</h2>
+                            <p id="p-right">Las mejores ofertas para recordar.</p>
+                        </div>
+                        <div class="feature">
+                            <img src="https://cdn4.iconfinder.com/data/icons/adiante-apps-app-templates-incos-in-grey/128/app_type_wedding_512px_GREY.png" alt="">
+                            <h2>Eventos unicos</h2>
+                            <p id="p-right">Tus eventos, serán recordados por siempre.</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -278,7 +339,7 @@
             <div class="container">
 
                 <div class="section-title center">
-                    <h2>Some of <strong>our Clients</strong></h2>
+                    <h2>Nuestros <strong> clientes</strong></h2>
                     <div class="line">
                         <hr>
                     </div>
@@ -316,7 +377,7 @@
     <div id="tf-works">
         <div class="container"> <!-- Container -->
             <div class="section-title text-center center">
-                <h2>Take a look at <strong>our services</strong></h2>
+                <h2> Ofertas <strong>del mes</strong></h2>
                 <div class="line">
                     <hr>
                 </div>
@@ -482,28 +543,30 @@
         <div class="overlay">
             <div class="container">
                 <div class="section-title center">
-                    <h2><strong>Our clients’</strong> testimonials</h2>
+                    <h2><strong>Testimonios</strong> </h2>
                     <div class="line">
                         <hr>
                     </div>
                 </div>
+
+
                 <div class="row">
                     <div class="col-md-8 col-md-offset-2">
                         <div id="testimonial" class="owl-carousel owl-theme">
                             <div class="item">
-                                <h5>This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</h5>
-                                <p><strong>Dean Martin</strong>, CEO Acme Inc.</p>
+                                <h5>Viaje por primera vez a San Miguel de Allende y con esta app pude conocer lugares asombrosos y disfrutar de la gastronomia de mi gusto..</h5>
+                                <p><strong>Alicia Zambrano</strong>, Guadalajara Jalisco.</p>
                             </div>
 
                             <div class="item">
-                                <h5>This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</h5>
+                                <h5>Me llene de cultura y pide visitar varios museos de mi interes gracias a esta aplicación.</h5>
+                                <p><strong>Martin Ramirez</strong>, Morelia Michoacan.</p>
+                            </div>
+                            <div class="item">
+                                <h5>Llegue a mi destino sin saber que hacer primero pero el itinerario que me sugirio esta app me fascino..</h5>
                                 <p><strong>Dean Martin</strong>, CEO Acme Inc.</p>
                             </div>
 
-                            <div class="item">
-                                <h5>This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</h5>
-                                <p><strong>Dean Martin</strong>, CEO Acme Inc.</p>
-                            </div>
                         </div>
                     </div>
                 </div>
@@ -520,7 +583,7 @@
                 <div class="col-md-8 col-md-offset-2">
 
                     <div class="section-title center">
-                        <h2>Feel free to <strong>contact us</strong></h2>
+                        <h2>Contactanos<strong></strong></h2>
                         <div class="line">
                             <hr>
                         </div>
@@ -560,7 +623,7 @@
     <nav id="footer">
         <div class="container">
             <div class="pull-left fnav">
-                <p>ALL RIGHTS RESERVED. COPYRIGHT © 2014. Designed by <a href="https://dribbble.com/shots/1817781--FREEBIE-Spirit8-Digital-agency-one-page-template">Robert Berki</a> and Coded by <a href="https://dribbble.com/jennpereira">Jenn Pereira</a></p>
+                <p>ALL RIGHTS RESERVED. COPYRIGHT © 2015. SWITCH 2015 Gto, México</p>
             </div>
             <div class="pull-right fnav">
                 <ul class="footer-social">
