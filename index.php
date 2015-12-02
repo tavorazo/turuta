081e1bec402fc839d573943928063421d8c71e67
Octavio <octavio.razo.verduzco@gmail.com>
2015-12-01 22:46:21 -0600

    front

--- index.php
+++ index.php
@@ -147,7 +147,7 @@
                     </div>
                 </div>
 
-                <div id="team" class="owl-carousel owl-theme row">
+                <div id="team" class="owl-carousel owl-theme row" style=" width: 75% !important;">
                     <!--div class="item">
                         <div class="thumbnail">
                             <img src="img/team/01.jpg" alt="..." class="img-circle team-img">
@@ -588,7 +588,7 @@
                             <hr>
                         </div>
                         <div class="clearfix"></div>
-                        <small><em>Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</em></small>            
+                        <small><em></em></small>            
                     </div>
 
                     <form>
